61fd935ea454661468f732cf19f3dca58d0102dc
Ronni Perez <rjperez94@gmail.com>
2017-08-23 19:20:09 +1200

    Initial commit

---
+++ Cell.java
@@ -0,0 +1,23 @@
+public class Cell {
+    public final int x;
+    public final int y;
+
+    private boolean visited = false;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public String toString() {
+        return String.format("%d,%d (%b)", x, y, visited);
+    }
+}
